bde28b1fbc2a82163b906ec8f9ff33d2c926a80c
likeSnake <2843586907@qq.com>
2023-06-09 16:15:23 +0800

    Initial commit

---
+++ app/src/main/java/net/ncie/cutemap/constant/Constant.java
@@ -0,0 +1,12 @@
+package net.ncie.cutemap.constant;
+
+public class Constant {
+    public static String APP_GET_URL = "https://123zaw7v8h.execute-api.us-west-2.amazonaws.com/default/map10022open";
+    public static String APP_POST_URL = "https://jktwft1zv7.execute-api.us-west-2.amazonaws.com/default/map10022event";
+
+    public static String AD_BEAN = "AD_BEAN";
+
+    public static Boolean isFocus = false;
+    public static String FIRST = "FIRST";
+
+}
